3c08fe0c1a8b5b1be7ffae6b33fd2ed4d0bd328c
josemigu <josemigu@student.42porto.com>
2025-04-04 19:13:24 +0100

    feat: ex27

---
+++ ex27/srcs/ft_display_file.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_display_file.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: josemigu <josemigu@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/04 16:27:41 by josemigu          #+#    #+#             */
+/*   Updated: 2025/04/04 18:53:37 by josemigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+bool	process_file(char *file)
+{
+	int		fd;
+	char	content[255];
+	int		bytes_read;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (false);
+	bytes_read = read(fd, content, 254);
+	while (bytes_read > 0)
+	{
+		content[bytes_read] = '\0';
+		ft_putstr(content);
+		bytes_read = read(fd, content, 254);
+	}
+	close(fd);
+	return (true);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		ft_putstr("File name missing.\n");
+		return (1);
+	}
+	if (argc > 2)
+	{
+		ft_putstr("Too many arguments.\n");
+		return (1);
+	}
+	if (!process_file(argv[1]))
+	{
+		ft_putstr("Cannot read file.\n");
+		return (1);
+	}
+	return (0);
+}
